077777bad4f5b3ca04378496b83512f63d5d007e
Lulwah Almisfer <67158449+LulwahAlmisfer@users.noreply.github.com>
2023-08-04 11:02:30 +0300

    add label + arrange folders

--- CoordinatorPattern_UIKit/VCs/StartVC.swift
+++ CoordinatorPattern_UIKit/VCs/StartVC.swift
@@ -13,10 +13,30 @@ class StartVC: UIViewController, Coordinating {
     let redButton  = customButton(color: .red, title: "red VC")
     let blueButton  = customButton(color: .blue, title: "blue VC")
 
+    let label = UILabel(frame: .zero)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureLabel()
         configureButtons()
     }
+    
+    func configureLabel() {
+        view.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Choose where to navigate 🚗"
+        label.textColor = .systemGray
+        label.textAlignment = .center
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            label.heightAnchor.constraint(equalToConstant: 50),
+         
+ 
+        ])
+    }
+    
     
     func configureButtons() {
         redButton.addTarget(self, action: #selector(redButtonClicked), for: .touchUpInside)
@@ -26,7 +46,7 @@ class StartVC: UIViewController, Coordinating {
         
 
         NSLayoutConstraint.activate([
-            redButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            redButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -200),
             redButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
             redButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
             redButton.heightAnchor.constraint(equalToConstant: 50),
